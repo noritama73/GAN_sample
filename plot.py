addfd5f64da7d5f6f561b4c765be7b5463a1a8c2
noritama73 <takuma.shioiri.eiko.rbb@gmail.com>
2022-05-31 22:36:53 +0900

    add plot.py

---
+++ plot.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv('./log.csv')
+
+data['plot_x'] = data['epoch'].astype(str) + '_' + data['iteration'].astype(str)
+
+plt.figure(figsize=(12,6))
+plt.plot(data['plot_x'], data['discriminator_loss'], label='discriminator_loss')
+plt.plot(data['plot_x'], data['generator_loss'], label='generator_loss')
+plt.xticks(np.arange(0, len(data)+1, 10), rotation=-50)
+plt.legend()
+plt.savefig('plot.png')
