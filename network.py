5e54078dace9c0ca15e1067f2c10d64eb37c8695
noritama73 <takuma.shioiri.eiko.rbb@gmail.com>
2022-06-01 12:32:46 +0900

    add batch normalize

--- network.py
+++ network.py
@@ -18,12 +18,14 @@ class discriminator(nn.Module):
     def __init__(self):
         super(discriminator, self).__init__()
         self.fc1 = nn.Linear(784, 512)
+        self.bn1 = nn.BatchNorm1d(512)
         self.fc2 = nn.Linear(512, 1)
         self.activation = nn.LeakyReLU(0.1)
 
     def forward(self, x):
         x = x.view(-1, 784)
         x = self.activation(self.fc1(x))
+        x = self.bn1(x)
         x = self.fc2(x)
         return nn.Sigmoid()(x)
 
@@ -32,13 +34,17 @@ class generator(nn.Module):
     def __init__(self):
         super(generator, self).__init__()
         self.fc1 = nn.Linear(128, 1024)
+        self.bn1 = nn.BatchNorm1d(1024)
         self.fc2 = nn.Linear(1024, 2048)
+        self.bn2 = nn.BatchNorm1d(2048)
         self.fc3 = nn.Linear(2048, 784)
         self.activation = nn.ReLU()
 
     def forward(self, x):
         x = self.activation(self.fc1(x))
+        x = self.bn1(x)
         x = self.activation(self.fc2(x))
+        x = self.bn2(x)
         x = self.fc3(x)
         x = x.view(-1, 1, 28, 28)
         return nn.Tanh()(x)
